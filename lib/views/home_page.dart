9d48af5f4248049afa34f3b59ebef907e92b49f1
somrat <somrat945hossain@gmail.com>
2024-09-05 01:42:27 +0600

    home page done

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_json_test/views/output_tow_page.dart';
+
+import 'output_one_page.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -6,23 +9,45 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 39, 51, 40),
       appBar: AppBar(
-        title: Text("Flutter Josn Test"),
+        title: const Text("Flutter Josn Test"),
         centerTitle: true,
       ),
       body: Center(
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            MaterialButton(onPressed: (){
-
-            },
-            child: Text("Output 1"),
+            MaterialButton(
+              color: Colors.white,
+              onPressed: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (_) => const OutputOnePage()));
+              },
+              child: const Text(
+                "Output 1",
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.w700,
+                ),
+              ),
             ),
-            SizedBox(height: 20,),
-            MaterialButton(onPressed: (){
-
-            },
-            child: Text("Output 2"),
+            const SizedBox(
+              height: 20,
+            ),
+            MaterialButton(
+              color: Colors.white,
+              onPressed: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (_) => const OutputTowPage()));
+              },
+              child: const Text(
+                "Output 2",
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.w700,
+                ),
+              ),
             )
           ],
         ),
---
+++ lib/views/output_tow_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class OutputTowPage extends StatelessWidget {
+  const OutputTowPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
